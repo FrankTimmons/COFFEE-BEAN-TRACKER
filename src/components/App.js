abacd06f74a91ca509f01d66e95db429507d34d3
FrankTimmons <franktimmonspdx@gmail.com>
2022-09-09 10:48:43 -0700

    Create header component and render it in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
     <React.Fragment>
+      <Header />
     </React.Fragment>
   );
 }
