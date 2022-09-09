0561cdc3f3aeebbe732eead49598247fbe944bbc
FrankTimmons <franktimmonspdx@gmail.com>
2022-09-09 14:16:55 -0700

    Add a BeanForm component for making new beans.

---
+++ src/components/BeanForm.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+
+function BeanForm(props) {
+
+  function handleNewBeanFormSubmission(event) {
+    event.preventDefault();
+    props.onNewBeanCreation(
+      {
+        name: event.target.name.value, 
+        roast: event.target.roast.value, 
+        origin: event.target.issue.value,
+        price: event.target.issue.value,
+        pounds: 130,
+        id: v4()
+      }
+    )
+  }
+
+  return (
+    <React.Fragment>
+      <form onSubmit={handleNewBeanFormSubmission}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Bean Name' />
+        <input
+          type='text'
+          name='roast'
+          placeholder='Roast' />
+        <input
+          type='text'
+          name='origin'
+          placeholder='Origin' />
+        <input
+          type='number'
+          name='price'
+          placeholder='Price per Pound' />
+        <button type='submit'>Add new bean type!</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+BeanForm.propTypes = {
+  onNewBeanCreation: PropTypes.func
+};
+
+export default BeanForm;
